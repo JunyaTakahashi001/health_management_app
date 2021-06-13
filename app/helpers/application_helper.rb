4614db13a8950d0aa543d53636549e795f850ad7
*2lIAdsYiQz <j.takahashi@stageinc.jp>
2021-06-13 20:58:18 +0900

    異常時文字色変更追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def text_color(v)
+        if v == "あり"
+            "text_color"
+        end
+    end
+    def text_color_temp(temp)
+        if 36.9 < temp
+            "text_color"
+        end
+    end
 end
