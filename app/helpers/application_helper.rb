4594b70016ac40d5b97a087ec0a622032798bb0d
*2lIAdsYiQz <j.takahashi@stageinc.jp>
2021-06-14 21:55:19 +0900

    indexビューで一月分ループを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,19 @@ module ApplicationHelper
             "text_color"
         end
     end
+    def end_of_month
+        date = @month
+        days = date.end_of_month
+        days = days.day
+    end
+
+    def first_day
+        date = @month
+        first_day = date.beginning_of_month
+    end
+
+    def last_day
+        date = @month
+        last_day = date.end_of_month
+    end
 end
